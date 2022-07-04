78a9deb5a604a34ced52689b9b4b50de15738cb8
grr20186938 <paulasucla@hotmail.com>
2022-07-04 02:49:44 -0300

    criação de navbar pós login; adição do bootstrap

--- projeto-dac/src/app/app-routing.module.ts
+++ projeto-dac/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginScreenComponent } from './login';
+import { HomeComponent } from './home';
 
 const routes: Routes = [
-  {path:'', component: LoginScreenComponent}
+  {path:'', component: LoginScreenComponent},
+  { path: 'home',
+    component: HomeComponent }
 ];
 
 @NgModule({
